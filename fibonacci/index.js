d2eacd98ca484920add2e8f3e99ee810e242a6ac
Brit Butler <redline6561@gmail.com>
2016-08-03 00:02:18 -0400

    Do the memoization 'in the right place'.

--- fibonacci/index.js
+++ fibonacci/index.js
@@ -1,11 +1,3 @@
-var fib = function (n) {
-  if (n == 1 || n == 0) {
-    return n;
-  } else {
-    return fib(n-1) + fib(n-2);
-  }
-};
-
 var fastFib = function (n) {
   return fibIter(1, 0, n);
 };
@@ -18,17 +10,21 @@ var fibIter = function (a, b, count) {
   }
 };
 
-Function.prototype.memoize = function(){
+Function.prototype.memoize = function() {
   var self = this, cache = {};
-  return function( arg ){
-    if(arg in cache) {
+  return function(arg) {
+    if (arg in cache) {
       return cache[arg];
     } else {
-      return cache[arg] = self( arg );
+      return cache[arg] = self(arg);
     }
   };
 };
 
-var memoizedFib = fib.memoize();
-var otherMemoFib = fib.memoize();
-
+var memoFib = (function (n) {
+  if (n == 1 || n == 0) {
+    return n;
+  } else {
+    return memoFib(n-1) + memoFib(n-2);
+  }
+}).memoize();
